86f0daad28d3ead5f058887f6cf82f4603647068
Yist Lin <yishen992@gmail.com>
2015-06-29 20:26:09 +0800

    edit bank_unordered_map.h and bank_unordered_map.cpp, without find()

--- src/bank_unordered_map.h
+++ src/bank_unordered_map.h
@@ -2,14 +2,25 @@
 // Copyright 2015-06 dsa_final15
 // This is the bank system implemented with unordered map
 
+#ifndef SRC_BANK_UNORDERED_MAP_H_
+#define SRC_BANK_UNORDERED_MAP_H_
+
 #include <iostream>
+#include <vector>
+#include <algorithm>
 #include <unordered_map>
 #include "account/account.h"
-#include "recommend.h"
 using std::unordered_map;
 using std::make_pair;
 using std::string;
+using std::vector;
+using std::pair;
 using std::cout;
+
+#define RECOMMEND_SIZE 10
+#define MAX_STRING_SIZE 100
+
+typedef vector<string> RecommendId;
 
 typedef unordered_map<std::string, Account> UMap;
 
@@ -29,8 +40,21 @@ class BankUMap {
   void findAccount(const string& reg_exp);
   void searchHistory(const string& id);
 
+  int max_num(const int a, const int b);
+  int min_num(const int a, const int b);
+  int abs_num(const int a);
+  char next_char(const char);
+  char prev_char(const char);
+  int computeScoring(const string&, const string&);
+  void getRecommend(const string&, RecommendId&);
+  void runRecommend(string, string, int, RecommendId&, int, int);
+
+  void existRecommend(const string&, RecommendId&);
+
  private:
   string current_login_user;
   UMap umap; // unordered_map<std::string, Account>
   TransferLog lg; // this is the log record system with time-record
 };
+
+#endif // SRC_BANK_UNORDERED_MAP_H_
